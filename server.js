e32320534ba621bf7d6ed02b414b42cd819ca53e
Atlas <jack@bentsen.us>
2021-08-16 15:59:10 -0700

    moving routes to server file

--- server.js
+++ server.js
@@ -15,7 +15,40 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workouttrackerdb", { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
+
+app.get('/stats', (req, res) => {
+    try {
+        
+    } catch (error) {
+        console.log(error);
+        res.json(error);
+    }
+});
+
+app.get('/exercise/?', (req, res) => {
+    try {
+        db.Workout.find({}).populate('Exercise')
+        .then((data)=> {
+            res.json(data);
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json(error);
+    }
+});
+
+app.get('/api/workouts', (req, res) => {
+    try {
+        db.Workout.find({}).populate('Exercise')
+        .then(data => {
+            res.json(data);
+        })
+    } catch (error) {
+        console.log(error);
+        res.json(error);
+    }
+})
 
 app.listen(PORT, () => {
     console.log(`App running on port ${PORT}!`);
